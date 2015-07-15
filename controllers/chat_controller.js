2b4fa9a4389f5e82a69e3557c53cd6edc2300589
Alberto Alcaraz <jaalcarazz@yahoo.com>
2015-07-15 13:19:38 -0500

    Test

--- controllers/chat_controller.js
+++ controllers/chat_controller.js
@@ -2,14 +2,12 @@ var User = require('../models/user');
 
 module.exports = {
   index: function(req, res){
-    return res.render('home/chat', { id: req.user.facebook_id });
+    return res.render('home/chat', { id: req.user.facebook_id })
   },
 
   users: function(req, res){
     var users = User.find({ online: true }, function(err, users){
-      if(err){ 
-        return res.status(500).json({ error: err.errors });
-      }
+      if(err){ return res.status(500).json({ error: err.errors }); }
 
       return res.json(users);
     });
